1d34fa75cfeacd2f663523c748a9cd0e1340d1b6
pduchesne <pduchesne@gmail.com>
2022-04-25 18:54:36 +0200

    update pages

--- pages/types/index.d.ts
+++ pages/types/index.d.ts
@@ -1,8 +1,40 @@
 import "main.scss";
+import * as rdflib from "rdflib";
 export declare const EditFile: (props: {
     fileUri: string;
 }) => JSX.Element;
-export declare const BrowseContent: () => JSX.Element;
-export declare const WelcomePage: () => JSX.Element;
-export declare const Editor: () => JSX.Element;
+export declare const EditActions: (props: {
+    graph: rdflib.Store;
+    onChange: (graph: rdflib.Store) => void;
+}) => JSX.Element;
+export declare const EditAction: (props: {
+    graph: rdflib.Store;
+    actionUri: string;
+    onChange: (graph: rdflib.Store) => void;
+}) => JSX.Element;
+/**
+ * Display the content of a folder resource
+ */
+export declare const DisplayFolder: (props: {
+    folderUri: string;
+    onSelect?: ((uri: string) => void) | undefined;
+}) => JSX.Element;
+/**
+ * Display the content of a resource, either folder or file.
+ * If no uri is provided, use the root folder of the current user's storage
+ */
+export declare const BrowseContent: (props: {
+    uri?: string;
+}) => JSX.Element;
+export declare const MainPage: () => JSX.Element;
 export declare const App: () => JSX.Element;
+export declare const MONACO_TYPE_MAP: {
+    "text/turtle": string;
+    "application/json": string;
+    default: undefined;
+};
+export declare const MonacoEditor: (props: {
+    text: string;
+    contentType?: string | undefined;
+    onChange?: ((newText: string) => void) | undefined;
+}) => JSX.Element;
